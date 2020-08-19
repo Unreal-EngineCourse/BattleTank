d0cce8cb7434b05d5b46de2539a8f3622fc53e65
hamza_boulandoum <hamzaboulandoum@gmail.com>
2020-08-19 15:07:53 +0100

    BT05 Creating and deleting landscapes

---
+++ BattleTankUnreal/Source/BattleTankUnreal/BattleTankUnrealGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleTankUnrealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANKUNREAL_API ABattleTankUnrealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
