2a59b0c469a4d09a15224f2d1c076c5a8de89596
hamza_boulandoum <hamzaboulandoum@gmail.com>
2020-08-29 14:34:32 +0100

    FINDING THE TANK AND OUTPUTING THE RESULTS Also adding classes and code

---
+++ BattleTankUnreal/Source/BattleTankUnreal/TankPlayerController.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankPlayerController.h"
+
+TankPlayerController::TankPlayerController()
+{
+}
+
+TankPlayerController::~TankPlayerController()
+{
+}
---
+++ BattleTankUnreal/Source/BattleTankUnreal/TankPlayerController2.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankPlayerController2.h"
+
+
+void ATankPlayerController2::BeginPlay() {
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Error, TEXT("Player Controller Not Posessinng A Tank  "));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Player controller Possessing : %s"), *ControlledTank->GetName());
+	}
+
+}
+ATank* ATankPlayerController2::GetControlledTank() const {
+
+	return Cast<ATank>(GetPawn());
+}
